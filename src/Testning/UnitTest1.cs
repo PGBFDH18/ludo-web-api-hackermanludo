ed7a23a9b65067406043d2b2bb1c61f852171296
olssonolof <44023615+olssonolof@users.noreply.github.com>
2019-01-22 14:25:44 +0100

    Added more tests

--- src/Testning/UnitTest1.cs
+++ src/Testning/UnitTest1.cs
@@ -35,5 +35,35 @@ namespace Testning
 
             Assert.Equal(question, answer);
         }
+
+
+
+        [Fact]
+        public void NextTurn()
+        {
+            var game = new LudoEngine(2);
+
+
+
+            var question = game.NextTurn();
+            var answer = new string[] { "Blue", "3" };
+
+            Assert.Equal(question, answer);
+        }
+
+        [Fact]
+        public void StartGameWihTwoPlayer()
+        {
+            var game = new LudoEngine(2);
+
+
+            Assert.Equal(2, game.PlayersList.Count);
+
+            Assert.Equal(40, game.TileList.Count);
+
+            Assert.Equal(5, game.FinalStretch.Count);
+
+            Assert.Equal(4, game.PlayersList[0].Pieces.Count);
+        }
     }
 }
